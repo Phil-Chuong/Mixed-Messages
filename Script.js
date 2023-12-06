38324ea5c19c05dfd04f1aa085fb552c63f42ad0
Phil-Chuong <68432036+Phil-Chuong@users.noreply.github.com>
2023-12-06 18:06:57 +0000

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,39 @@
+// Javascripts function
+
+const Tops = ['Dress', 'T-Shirt', 'Vest', 'Polo-Neck', 'Naked'];
+const Bottoms = ['Skirt', 'Trousers', 'Jeans', 'Leggings', 'Shorts', 'Nothing'];
+const Footwear = ['High-Heels', 'Trainers', 'Sandals', 'Boots'];
+
+function handleTops() {
+
+    const randomIndex = Math.floor(Math.random() * Tops.length);
+
+    const randomItem = Tops[randomIndex];
+
+    document.getElementById('results-tops').innerText = `${randomItem}`
+};
+
+function handleBottoms() {
+
+    const randomIndex = Math.floor(Math.random() * Bottoms.length);
+
+    const randomItem = Bottoms[randomIndex];
+
+    document.getElementById('results-bottoms').innerText = `${randomItem}`
+};
+
+function handleFootwear() {
+
+    const randomIndex = Math.floor(Math.random() * Footwear.length);
+
+    const randomItem = Footwear[randomIndex];
+
+    document.getElementById('results-footwear').innerText = `${randomItem}`
+};
+
+function handleReset() {
+    // Reset the displayed results or perform other cleanup tasks
+    document.getElementById('results-tops').innerText = '';
+    document.getElementById('results-bottoms').innerText = '';
+    document.getElementById('results-footwear').innerText = '';
+}
